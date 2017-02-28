b45e130f0ba635c81426246d4b309b76b4428f9d
Piklu Sutradhar <Piklu Sutradhar>
2017-02-27 19:01:53 -0600

    tried to do some

--- ass2.c
+++ ass2.c
@@ -17,13 +17,13 @@
 //-------------------------------------------------------------------------------------
 // CONSTANTS and TYPES
 //-------------------------------------------------------------------------------------
-typedef struct game{
+struct game{
     char title[50];
     char board[100][100];
     int columns;
     int rows;
     int moves;
-}Game;
+};
 //-------------------------------------------------------------------------------------
 // VARIABLES
 //-------------------------------------------------------------------------------------
@@ -35,29 +35,42 @@ typedef struct game{
 //-------------------------------------------------------------------------------------
 // FUNCTIONS
 //-------------------------------------------------------------------------------------
-
+void print(struct game *game1);
 int main( int argc, char *argv[] )
 {
-    Game game;
-    FILE *file;
+    struct game game1;
+    FILE *file = NULL;
     char *title;
-    if(argc < 2){
-        printf("Enter the file name");
-        return EXIT_SUCCESS;
-    }
-    else{
-        file = fopen(argv[1],"r");
-        //char line[100];
-        fgets(game->title, 100, file);
-        fscanf(file,"%d %d %d",game->rows, game->columns, game)
-        while(line != EOF){
-            char c = fgetc(file);
-            if(c == '*'){
-                while(fgetc(file)!= '\n'){
-                  title = fgetc(file);   
-                }
-                title = fgetc(file);
+//    if(argc < 2){
+//        printf("Enter the file name");
+//        return EXIT_SUCCESS;
+//    }
+   // else{
+        file = fopen("sample.txt","r");
+        char line[100];
+        fgets(line, 100, file);
+        printf("%s",line);
+	    fscanf(file,"%d %d %d",&game1.rows, &game1.columns, &game1.moves);
+        int r, c;
+        for(r = 0; r < game1.rows; r++){
+            for(c = 0; c < game1.columns; c++){
+                game1.board[r][c] = fgetc(file);
+                printf("%c",game1.board[r][c]);
             }
         }
-  return EXIT_SUCCESS;
+        //file.close();
+    //}
+    print(&game1);
+      return EXIT_SUCCESS;
 }
+void print(struct game *game1){
+printf("%s", game1->title);
+        int r, c;
+        for(r = 0; r < game1->rows; r++){
+            for(c = 0; c < game1->columns; c++){
+                printf("%c", game1->board[r][c]);
+            }
+            printf("\n");
+        }
+}
+
