8355fea04d358a5b0d69ac1997ee51e5ffa4bec1
Piklu Sutradhar <Piklu Sutradhar>
2017-03-01 05:58:49 -0600

    storing in game is woriking

--- ass2.c
+++ ass2.c
@@ -46,36 +46,39 @@ int main( int argc, char *argv )
     char firstChar;
     char title[LINE_LENGTH];
     FILE * file;
-    file = fopen("std.txt.txt", "r" );
+    file = fopen("data.txt.txt", "r" );
     firstChar = fgetc(file);
     while(firstChar){
         if(firstChar == '*'){
             int i,j;
             fgets(game1.title,LINE_LENGTH,file);
-            printf("%s", game1.title);
+            //printf("%s", game1.title);
             fscanf(file, "%d %d %d", &game1.rows, &game1.columns, &game1.moves);
-            printf("%d %d %d",game1.rows, game1.columns, game1.moves);
+            //printf("%d %d %d",game1.rows, game1.columns, game1.moves);
             char command[100];
             for(i=0;i<game1.rows;i++){
                 for(j=0;j<game1.columns;j++){
                     game1.board[i][j] = fgetc(file);
-                    printf("%c",game1.board[i][j])
+                    //printf("%c",game1.board[i][j]);
                 }
                 fgetc(file);
             }
+            print(&game1);
             fgetc(file);
             fgets(command, 100, file);
+
         }
         firstChar = fgetc(file);
     }
+
     fclose(file);
     return EXIT_SUCCESS;
 }
-void setBoard(GAME *game1; int rows, int columns){
+/*void setBoard(GAME *game1; int rows, int columns){
     game1->board = char ga
 
-}
-void print(struct game *game1)
+*/
+void print(GAME *game1)
 {
     printf("%s", game1->title);
     int r, c;
