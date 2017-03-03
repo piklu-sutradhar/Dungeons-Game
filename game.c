e2484d170c4633a94250057ef3363947713c7727
Piklu Sutradhar <Piklu Sutradhar>
2017-03-03 02:30:03 -0600

    equation is working

--- game.c
+++ game.c
@@ -42,16 +42,19 @@ typedef struct game
 //-------------------------------------------------------------------------------------
 // FUNCTIONS
 //-------------------------------------------------------------------------------------
+int min(int x, int y);
+int max(int min, int x, int y);
+BOOL wallPresent(GAME * game1, int yPosition, int xPosition, int yLight, int xLight);
 void makeBorder(GAME *game1,int rows, int columns);
 void print(GAME *game1,int r,int c);
-int main( int argc, char *argv )
+int main( int argc, char *argv[] )
 {
     GAME game1;
     char firstChar;
     int moves;
     char title[LINE_LENGTH];
     FILE * file;
-    file = fopen("data.txt.txt", "r" );
+    file = fopen("std.txt.txt", "r" );
     firstChar = fgetc(file);
     while(firstChar){
         if(firstChar == '*'){
@@ -146,30 +149,33 @@ void print(GAME *game1,int rows, int columns)
     {
         for(c = 0; c < game1->columns+2; c++){
                 distance = sqrt(pow(((double)(r-rows)),2) + pow(((double)(c-columns)),2));
-                if(r == 0 || r == game1->rows+1 || c == 0 || c == game1->columns+1){
+                if(r == 0 || r == game1->rows+1 || c == 0 || c == game1->columns+1 || game1->gameBoard[r][c] == WALL || (wallPresent(game1,r,c,rows,columns) == TRUE)){
                    printf("%c", game1->gameBoard[r][c] );
                 }
-            else if((distance < 1.00) && (game1->gameBoard[r][c] != WALL)){
+                //else if ((wallPresent(game1,r,c,rows,columns) == FALSE))
+            else if((distance < 1.00)){
                     game1->gameBoard[r][c] = SOMEPOINT;
                 printf("%c", '%');
             }
-            else if ((distance < 2.00) && (game1->gameBoard[r][c] != WALL)){
+            else if (distance < 2.00){
                game1->gameBoard[r][c] = SOMEPOINT;
                 printf("%c", '#');
             }
-            else if ((distance < 3.00) && (game1->gameBoard[r][c] != WALL)){
+            else if (distance < 3.00){
                  game1->gameBoard[r][c] = SOMEPOINT;
                 printf("%c", '=');
             }
-            else if ((distance < 4.00) && (game1->gameBoard[r][c] != WALL)){
+            else{
+                    if(distance < 4.00){
                 game1->gameBoard[r][c] = SOMEPOINT;
                 printf("%c", '-');
             }
             else{
+               printf("%c", game1->gameBoard[r][c]);
+            }
+            }
+            /*else{
             switch(game1->gameBoard[r][c]){
-            case WALL:
-                printf("%c", WALL);
-                break;
             case NEVER:
                 printf("%c", NEVER);
                 break;
@@ -178,9 +184,53 @@ void print(GAME *game1,int rows, int columns)
                 break;
             }
             //printf("%c", game1->board[r][c]);
-        }
+        }*/
         }
         printf("\n");
     }
 }
-
+BOOL wallPresent(GAME * game1, int yPosition, int xPosition, int yLight, int xLight){
+    BOOL result = FALSE;
+    int i;
+int minX, maxX, minY, maxY;
+minX = min(xPosition, xLight);
+maxX = max(minX, xPosition, xLight);
+minY = min(yPosition, yLight);
+maxY = max(minY, yPosition, yLight);
+if(minX == maxX){
+    for(i = minY + 1; i < maxY; i++){
+        if(game1->gameBoard[i][minX] == WALL){
+            return TRUE;
+        }
+    }
+}
+else if (minY == maxY){
+     for(i = minX + 1; i < maxX; i++){
+        if(game1->gameBoard[minY][i] == WALL){
+            return TRUE;
+        }
+    }
+}
+else{
+    double slope = (double)(yLight - yPosition)/(double)(xLight - xPosition);
+    double intersect = yLight - (slope * xLight);
+    for(i = minX + 1; i < maxX; i++){
+            int y = slope * i + intersect;
+        if(game1->gameBoard[y][i] == WALL){
+            return TRUE;
+            }
+        }
+    return FALSE;
+    }
+}
+int min(int x, int y){
+    if (x < y){
+        return x;
+    }
+    else{
+        return y;
+    }
+}
+int max(int min, int x, int y){
+return x+y-min;
+}
