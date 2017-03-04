5c1fa1616b57719025614da2e26e1dce604a7fbd
Piklu Sutradhar <Piklu Sutradhar>
2017-03-03 18:19:51 -0600

    Fixed on Linux  machine

--- game.c
+++ game.c
@@ -13,14 +13,13 @@
 #include<ctype.h>
 #include<string.h>
 #include<math.h>
+#include<assert.h>
 
+#define DNDEBUG
 #define LINE_LENGTH 50
 //-------------------------------------------------------------------------------------
 // CONSTANTS and TYPES
 //-------------------------------------------------------------------------------------
-int ROWS = 100;
-int COLUMNS = 100;
-int MOVES = 100;
 typedef enum state {NEVER = ' ',SOMEPOINT = ',',WALL = '!',LIGHTSOURCE = '%'} State;
 typedef enum border {CORNER = '+', VERTICAL = '-', HORIZONTAL = '|'} Border;
 typedef enum Bool {FALSE, TRUE} BOOL;
@@ -38,40 +37,45 @@ typedef struct game
 //-------------------------------------------------------------------------------------
 // PROTOTYPES
 //-------------------------------------------------------------------------------------
-
-//-------------------------------------------------------------------------------------
-// FUNCTIONS
-//-------------------------------------------------------------------------------------
+void makeMoves(GAME * game1,int r, int c, char command[]);
 int min(int x, int y);
 int max(int min, int x, int y);
 BOOL wallPresent(GAME * game1, int yPosition, int xPosition, int yLight, int xLight);
 void makeBorder(GAME *game1,int rows, int columns);
-void print(GAME *game1,int r,int c);
+void print(GAME *game1,int r,int c, int move);
+//-------------------------------------------------------------------------------------
+// FUNCTIONS
+//-------------------------------------------------------------------------------------
+
 int main( int argc, char *argv[] )
 {
     GAME game1;
-    char firstChar;
     int moves;
-    char title[LINE_LENGTH];
     FILE * file;
-    file = fopen("std.txt.txt", "r" );
-    firstChar = fgetc(file);
-    while(firstChar){
-        if(firstChar == '*'){
+    if (argc < 2){
+    printf("One Argument Expected\n");
+    return EXIT_FAILURE;
+    }
+    else if (argc > 2){
+    printf("More Than Expected Argument Entered\n");
+    return EXIT_FAILURE;
+    }
+    else{
+    assert(argc == 2);
+    file = fopen(argv[1],"r");
+    while(fgets(game1.title,LINE_LENGTH,file)){
+        if(game1.title[0] == '*'){
             int i,j;
-            fgets(game1.title,LINE_LENGTH,file);
-            //printf("%s", game1.title);
+            printf("%s", game1.title);
             fscanf(file, "%d %d %d", &game1.rows, &game1.columns, &moves);
             fgetc(file);
-            //printf("%d %d %d",game1.rows, game1.columns, game1.moves);
+            fgetc(file);
             char command[100];
-            //game1.gameBoard = int[game1.rows][game1.columns];
             makeBorder(&game1,game1.rows, game1.columns);
             int r=0, c=0;
             for(i=1;i<game1.rows+1;i++){
                 for(j=1;j<game1.columns+1;j++){
                         char ch = fgetc(file);
-                        //game1.board[i][j] = ch;
                     if(ch == '@'){
                             game1.gameBoard[i][j] = LIGHTSOURCE;
                         r = i;
@@ -86,16 +90,30 @@ int main( int argc, char *argv[] )
                     else{
                         game1.gameBoard[i][j] = SOMEPOINT;
                     }
-
-                    //printf("%c",game1.board[i][j]);
                 }
                 fgetc(file);
+                fgetc(file);
             }
-            printf("Move %d: \n", 0);
-            print(&game1,r,c);
-            //fgetc(file);
+            print(&game1,r,c,0);
             fgets(command, 100, file);
-            for(i = 0; i < strlen(command); i++){
+            makeMoves(&game1,r,c, command);
+        }
+    }
+    fclose(file);
+    return EXIT_SUCCESS;
+    }
+}
+void makeMoves(GAME * game1,int r, int c, char command[]){
+int i;
+int moves = strlen(command);
+int start = 0;
+/*#ifdef DNDEBUG
+start = moves - 10;
+#endif
+#ifndef DNDEBUG
+ start = 0;
+ #endif*/
+            for(i = start; i < moves; i++){
                     if(command[i] == '>'){
                         c++;
                     }
@@ -108,19 +126,13 @@ int main( int argc, char *argv[] )
                     else{
                         c--;
                     }
-                    printf("Move %d: \n", i+1);
-             print(&game1,r,c);
+             print(game1,r,c,i+1);
             }
-
-        }
-        firstChar = fgetc(file);
-    }
-
-    fclose(file);
-    return EXIT_SUCCESS;
+	    //#endif
 }
 void makeBorder(GAME *game1,int rows, int columns){
 int r, c;
+
     for(r = 0; r < game1->rows+2; r++)
     {
         for(c = 0; c < game1->columns+2; c++){
@@ -140,19 +152,18 @@ int r, c;
         }
     }
 }
-void print(GAME *game1,int rows, int columns)
+void print(GAME *game1,int rows, int columns, int move)
 {
-    //printf("%s", game1->title);
+    printf("Move %d\n", move);
     int r, c;
     double distance;
     for(r = 0; r < game1->rows+2; r++)
     {
         for(c = 0; c < game1->columns+2; c++){
                 distance = sqrt(pow(((double)(r-rows)),2) + pow(((double)(c-columns)),2));
-                if(r == 0 || r == game1->rows+1 || c == 0 || c == game1->columns+1 || game1->gameBoard[r][c] == WALL || (wallPresent(game1,r,c,rows,columns) == TRUE)){
+                if(move == 0 || r == 0 || r == game1->rows+1 || c == 0 || c == game1->columns+1 || game1->gameBoard[r][c] == WALL || (wallPresent(game1,r,c,rows,columns) == TRUE)){
                    printf("%c", game1->gameBoard[r][c] );
                 }
-                //else if ((wallPresent(game1,r,c,rows,columns) == FALSE))
             else if((distance < 1.00)){
                     game1->gameBoard[r][c] = SOMEPOINT;
                 printf("%c", '%');
@@ -173,55 +184,44 @@ void print(GAME *game1,int rows, int columns)
             else{
                printf("%c", game1->gameBoard[r][c]);
             }
-            }
-            /*else{
-            switch(game1->gameBoard[r][c]){
-            case NEVER:
-                printf("%c", NEVER);
-                break;
-            default:
-                printf("%c", SOMEPOINT);
-                break;
-            }
-            //printf("%c", game1->board[r][c]);
-        }*/
+          }
         }
         printf("\n");
     }
 }
 BOOL wallPresent(GAME * game1, int yPosition, int xPosition, int yLight, int xLight){
-    BOOL result = FALSE;
-    int i;
+int i;
+BOOL result = FALSE;
 int minX, maxX, minY, maxY;
 minX = min(xPosition, xLight);
 maxX = max(minX, xPosition, xLight);
 minY = min(yPosition, yLight);
 maxY = max(minY, yPosition, yLight);
 if(minX == maxX){
-    for(i = minY + 1; i < maxY; i++){
+    for(i = minY + 1; i < maxY && result == FALSE; i++){
         if(game1->gameBoard[i][minX] == WALL){
-            return TRUE;
+            result = TRUE;
         }
     }
 }
 else if (minY == maxY){
-     for(i = minX + 1; i < maxX; i++){
+     for(i = minX + 1; i < maxX && result == FALSE; i++){
         if(game1->gameBoard[minY][i] == WALL){
-            return TRUE;
+            result = TRUE;
         }
     }
 }
 else{
     double slope = (double)(yLight - yPosition)/(double)(xLight - xPosition);
     double intersect = yLight - (slope * xLight);
-    for(i = minX + 1; i < maxX; i++){
+    for(i = minX + 1; i < maxX && result == FALSE; i++){
             int y = slope * i + intersect;
         if(game1->gameBoard[y][i] == WALL){
-            return TRUE;
+            result = TRUE;
             }
         }
-    return FALSE;
-    }
+  }
+  return result;
 }
 int min(int x, int y){
     if (x < y){
